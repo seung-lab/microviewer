dde168df876ceec90feb4f181450606acf49c8b3
William Silversmith <william.silversmith@gmail.com>
2023-06-15 15:27:44 -0400

    feat: support loading memmaps

--- microviewer_cli/cli.py
+++ microviewer_cli/cli.py
@@ -8,6 +8,19 @@ import click
 import microviewer
 import numpy as np
 
+class Tuple234(click.ParamType):
+  """A command line option type consisting of 3 comma-separated integers."""
+  name = 'tuple234'
+  def convert(self, value, param, ctx):
+    if isinstance(value, str):
+      try:
+        value = tuple(map(int, value.split(',')))
+      except ValueError:
+        self.fail(f"'{value}' does not contain a comma delimited list of 3 or 4 integers.")
+      if len(value) not in (2,3,4):
+        self.fail(f"'{value}' does not contain a comma delimited list of 3 or 4 integers.")
+    return value
+
 def root_file_ext(filename):
   filename, ext = os.path.splitext(filename)
 
@@ -38,12 +51,31 @@ def load_bytesio(filelike):
   
   return io.BytesIO(binary)
 
-def load(filename):
+# c/o https://stackoverflow.com/questions/64226337/is-there-a-way-to-read-npy-header-without-loading-the-whole-file
+def read_numpy_array_header(fobj):
+  version = np.lib.format.read_magic(fobj)
+  func_name = 'read_array_header_' + '_'.join(str(v) for v in version)
+  func = getattr(np.lib.format, func_name)
+  return func(fobj)
+
+def load_numpy(src, shape, dtype, order):
+  try:
+    with open(src, "rb") as f:
+      shape, forder, dtype = read_numpy_array_header(f)
+      order = "F" if forder else "C"
+  except ValueError:
+    if dtype is None or shape is None or order not in ("C", "F"):
+      raise
+    dtype = np.dtype(dtype)
+
+  return np.memmap(src, dtype=dtype, shape=shape, order=order, mode="r")
+
+def load(filename, shape, dtype, order):
   binary = load_bytesio(filename)
   ext = root_file_ext(filename)
 
   if ext == ".npy":
-    image = np.load(binary)
+    image = load_numpy(filename, shape, dtype, order)
   elif ext == ".nii":
     import nibabel as nib
     image = nib.load(filename)
@@ -58,14 +90,17 @@ def load(filename):
 @click.argument("segmentation", required=False, default=None)
 @click.option('--seg', is_flag=True, default=False, help="Display image as segmentation.", show_default=True)
 @click.option('--browser/--no-browser', default=True, is_flag=True, help="Open the dataset in the system's default web browser.", show_default=True)
-def main(image, segmentation, seg, browser):
+@click.option('--shape', type=Tuple234(), default=None, help="Set shape manually for memmaps.", show_default=True)
+@click.option('--dtype', type=str, default=None, help="Set dtype manually for memmaps.", show_default=True)
+@click.option('--order', type=str, default="F", help="Set order manually for memmaps.", show_default=True)
+def main(image, segmentation, seg, browser, shape, dtype, order):
   """
   View 3D images in the browser.
   """
   try:
-    image_np = load(image)
+    image_np = load(image, shape, dtype, order)
     if segmentation:
-      segmentation_np = load(segmentation)
+      segmentation_np = load(segmentation, shape, dtype, order)
   except ValueError:
     print("Data type not supported.")
     return
@@ -81,6 +116,3 @@ def main(image, segmentation, seg, browser):
       image_np, seg=seg, 
       browser=browser, cloudpath=image
     )
-
-
-
